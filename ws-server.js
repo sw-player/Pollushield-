db3e23320001cef6447852a2a7af00937020406b
Sangwoo Hahn <sangwoo2807@gmail.com>
2025-04-28 16:59:09 +0900

    ws-server.js edit

--- ws-server.js
+++ ws-server.js
@@ -1,22 +1,19 @@
-// 프로젝트 루트에 ws-server.js 파일로 생성하세요
-// (Pollushield-portfolio/ws-server.js)
+// ws-server.js
+const { Server: WebSocketServer } = require('ws');
 
-const { WebSocketServer } = require('ws');  // CommonJS 방식으로 변경
-
-// WebSocket 서버 포트 설정
 const wss = new WebSocketServer({ port: 4000 });
 console.log('WebSocket 서버가 포트 4000에서 실행 중');
 
 wss.on('connection', ws => {
   console.log('클라이언트 연결됨');
-
-  // 클라이언트로부터 받은 메시지를 그대로 되돌려 줍니다
+  
+  // 클라이언트로부터 받은 메시지를 다시 보냄
   ws.on('message', msg => {
     console.log('받은 메시지:', msg.toString());
     ws.send(msg);
   });
 
-  // 2초마다 테스트 이벤트 자동 전송 (옵션)
+  // 2초마다 테스트 메시지 전송
   const interval = setInterval(() => {
     const data = {
       time: Date.now(),
